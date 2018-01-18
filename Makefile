0708886d885fd0b8ac18013829d3997612422d43
omochimetaru <omochi.metaru@gmail.com>
2018-01-18 22:19:05 +0900

    Blake2 SwiftPM

--- Makefile
+++ Makefile
@@ -1,18 +1,14 @@
-LIBS_DIR := ${CURDIR}/Libs/macos
-SWIFT_FLAGS += -Xlinker "-L${LIBS_DIR}"
-SWIFT_FLAGS += -Xlinker "-lb2"
-
 .PHONY:	clean build test
 
 clean:
 	swift package clean
 
 build:
-	swift build ${SWIFT_FLAGS}
+	swift build
 
 test:
-	swift test ${SWIFT_FLAGS}
+	swift test
 
 xcode:
-	swift package ${SWIFT_FLAGS} generate-xcodeproj
+	swift package generate-xcodeproj
 
--- Package.swift
+++ Package.swift
@@ -9,11 +9,11 @@ let package = Package(
         .library(name: "RaiBlocksSwift", targets: ["RaiBlocksSwift"]),
         ],
     dependencies: [
-         .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.2"),
-    ],
+        .package(url: "https://github.com/omochi/Blake2-SwiftPM.git", from: "1.0.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "3.0.2"),
+        ],
     targets: [
-        .target(name: "libb2"),
-        .target(name: "Basic", dependencies: ["BigInt", "libb2"]),
+        .target(name: "Basic", dependencies: ["BigInt", "Blake2"]),
         .target(name: "RaiBlocksSwift", dependencies: ["Basic"]),
         .testTarget(name: "BasicTests", dependencies: ["Basic"]),
         .testTarget(name: "RaiBlocksSwiftTests", dependencies: ["RaiBlocksSwift"]),
